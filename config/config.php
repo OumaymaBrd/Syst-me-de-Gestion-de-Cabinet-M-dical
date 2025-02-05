6629109b2f1b9d502a44ccfbc42b5fc84efbdc58
OumaymaBrd <oumaymalovemaths>
2025-02-05 16:47:24 +0100

    Regler routes dans  config

--- config/config.php
+++ config/config.php
@@ -1,16 +1,11 @@
 <?php
 
 return [
-    'db' => [
-        'host' => $_ENV['DB_HOST'],
-        'name' => $_ENV['DB_NAME'],
-        'user' => $_ENV['DB_USER'],
-        'pass' => $_ENV['DB_PASS'],
-        'charset' => $_ENV['DB_CHARSET']
-    ],
-    'app' => [
-        'name' => 'Cabinet Médical',
-        'url' => $_ENV['APP_URL'],
-        'debug' => $_ENV['APP_DEBUG'] === 'true'
+    'database' => [
+        'host' => 'localhost',
+        'name' => 'cabinet_medical',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4'
     ]
 ];
