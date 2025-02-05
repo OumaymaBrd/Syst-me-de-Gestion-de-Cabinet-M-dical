c05b321dd126aac115c9a13085c3ad29db49423b
OumaymaBrd <oumaymalovemaths>
2025-02-05 12:40:00 +0100

    Ajouter la class abstract de controller dans core

---
+++ src/Views/Core/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Core;
+
+abstract class Controller
+{
+    protected function render($view, $data = [])
+    {
+        extract($data);
+        include __DIR__ . "/../Views/templates/header.php";
+        include __DIR__ . "/../Views/$view.php";
+        include __DIR__ . "/../Views/templates/footer.php";
+    }
+
+    protected function redirect($url)
+    {
+        header("Location: $url");
+        exit;
+    }
+}
