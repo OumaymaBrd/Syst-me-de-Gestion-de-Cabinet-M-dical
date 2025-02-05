5d6d45aa30f3239cfb3aa9f632ba27d16871237f
OumaymaBrd <oumaymalovemaths>
2025-02-05 16:29:21 +0100

    Ajouter dans index :  = Dotenv\Dotenv::createImmutable(__DIR__ . '/..'); ->load();

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Charger les variables d'environnement
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
-
 // Initialiser la session
 session_start();
 
@@ -18,3 +17,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // Dispatcher la requête
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
