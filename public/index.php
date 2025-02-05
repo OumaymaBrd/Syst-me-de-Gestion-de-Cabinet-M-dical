a61d9076fd4a9d59affd31d69455003218b77e8f
OumaymaBrd <oumaymalovemaths>
2025-02-05 16:28:11 +0100

    Ajouter dans insdex lemrequire_once __DIR__ . '/../vendor/autoload.php'

--- public/index.php
+++ public/index.php
@@ -14,6 +14,7 @@ $router = new App\Core\Router();
 
 // Charger les routes
 require_once __DIR__ . '/../config/routes.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Dispatcher la requête
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
